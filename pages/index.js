f80ed8a737fbe97b02565cac96f1da577ea57981
Patrick <51328607+unenunciate@users.noreply.github.com>
2021-10-01 20:06:18 -0400

    fixed  layout  logo

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
         </h1>
 
         <div className="flex flex-col items-center justify-center min-w-full -mb-8">
-          <div className="flex flex-col items-center justify-center w-full">
+          <div className="flex flex-col items-center justify-center w-full space-y-4">
             <button onClick={() => router.push("https://app.jokingon.com/login")} className="w-5/6 p-2 font-bold bg-black rounded-lg outline-none sm:w-2/3 md:w-1/2 lg:w-1/3 text-lemon-meringue ring-black ring-2 active:scale-75"> Application </button>
             <PopupButton
               id="ROgN6Csu"
@@ -81,7 +81,7 @@ export default function Home() {
           <img
             src="/avax.svg"
             alt="Avax Logo"
-            className="items-center w-24 h-10 fill-current text-lemon-meringue"
+            className="items-center w-24 h-10"
           />
         </a>
       </footer>
