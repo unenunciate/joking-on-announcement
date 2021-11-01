c8a0689daf3c214549bb7cdb016a58b8f681bc7f
Patrick <51328607+unenunciate@users.noreply.github.com>
2021-11-01 19:26:21 -0400

    polkadot svg and temp removed application button

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,15 @@ export default function Home() {
 
         <div className="flex flex-col items-center justify-center min-w-full -mb-8">
           <div className="flex flex-col items-center justify-center w-full space-y-4">
-            <button onClick={() => router.push("https://app.jokingon.com/login")} className="w-5/6 p-2 font-bold bg-black rounded-lg outline-none sm:w-2/3 md:w-1/2 lg:w-1/3 text-lemon-meringue ring-black ring-2 active:scale-75"> Application </button>
+            {false && (
+              <button
+                onClick={() => router.push("https://app.jokingon.com/login")}
+                className="w-5/6 p-2 font-bold bg-black rounded-lg outline-none sm:w-2/3 md:w-1/2 lg:w-1/3 text-lemon-meringue ring-black ring-2 active:scale-75"
+              >
+                {" "}
+                Application{" "}
+              </button>
+            )}
             <PopupButton
               id="ROgN6Csu"
               className="w-5/6 p-2 font-bold bg-black rounded-lg outline-none sm:w-2/3 md:w-1/2 lg:w-1/3 text-lemon-meringue ring-black ring-2 active:scale-75"
@@ -41,8 +49,9 @@ export default function Home() {
           <a className="text-center rounded-xl text-maximum-red focus:text-maximum-black sm:w-5/6 lg:w-1/2 hover:text-black">
             <h3 className="font-bold">NFT Raise</h3>
             <p className="text-sm">
-              To join our NFT Raise, please send the NFT(s), only ethereum mainnet
-              or Avax C-Chain accepted, that you would like to contribute to:
+              To join our NFT Raise, please send the NFT(s), only ethereum
+              mainnet or Avax C-Chain accepted, that you would like to
+              contribute to:
               <span className="block text-xs text-maximum-red hover:text-lemon-meringue">
                 0xD70ac44Eb0055d089aDF96F0318f3509C5C70cAa
               </span>
@@ -51,10 +60,11 @@ export default function Home() {
               <span className="block p-4 text-[8px] sm:text-xs">
                 Disclaimer: The secondary market value, or the value of a
                 particular NFT when we choose to sell it, doesn't contribute to
-                our decision of how many joke tokens to send you. Fungible tokens
-                are not a substitute for NFTs. Fungible tokens received will not result in
-                reciprocation of joke tokens. Any ERC20 tokens sent will not be
-                returned. This raise ends whenever we decide it does.
+                our decision of how many joke tokens to send you. Fungible
+                tokens are not a substitute for NFTs. Fungible tokens received
+                will not result in reciprocation of joke tokens. Any ERC20
+                tokens sent will not be returned. This raise ends whenever we
+                decide it does.
               </span>
             </p>
           </a>
@@ -71,7 +81,7 @@ export default function Home() {
       <footer className="flex items-center justify-center w-full h-[10%] p-2 bg-black">
         <a
           className="flex items-center"
-          href="https://www.avax.network/"
+          href="https://polkadot.network/"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -79,8 +89,8 @@ export default function Home() {
             Built On
           </div>
           <img
-            src="/avax.svg"
-            alt="Avax Logo"
+            src="/polkadot.svg"
+            alt="Polkadot Logo"
             className="items-center w-24 h-10"
           />
         </a>
